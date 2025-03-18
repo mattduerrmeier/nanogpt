a49b5ad221470529c81bcf51df728c7865d6bfe3
Mattias Dürrmeier <matt.duerr@pm.me>
2025-03-18 13:33:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from model import GPT, Config
+import torch
+from torch.nn import functional as F
+import tiktoken
+
+
+def inference(n_repeat: int = 5, max_length: int = 30, pretrained: bool = False, seed: int = 42) -> None:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"using device: {device}")
+
+    if pretrained:
+        model = GPT.from_pretrained("gpt2")
+        print("Loaded HF Pretrained GPT-2. Forward pass...")
+    else:
+        model = GPT(Config())
+        print("Loaded random GPT-2. Forward pass...")
+
+    model.eval()
+    model.to(device)
+
+    # the gpt-2 tokenizer (using OpenAI library)
+    # compression ratio: 3 to 1 (1000 char -> 300 tokens)
+    enc = tiktoken.get_encoding("gpt2")
+    tokens = enc.encode("Hello, I'm a language model,")
+    tokens = torch.tensor(tokens, dtype=torch.long)
+    tokens = tokens.unsqueeze(0).repeat(n_repeat, 1)
+
+    x = tokens.to(device)
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+    while x.size(1) < max_length:
+        with torch.no_grad():
+            logits = model(x)
+
+            logits = logits[:, -1, :]
+
+            probs = F.softmax(logits, dim=-1)
+            topk_probs, top_indices = torch.topk(probs, 50, dim=-1)
+            ix = torch.multinomial(topk_probs, 1)
+
+            xcol = torch.gather(top_indices, -1, ix)
+
+            x = torch.cat((x, xcol), dim=1)
+
+    for i in range(n_repeat):
+        tokens = x[i, :max_length].tolist()
+        decoded = enc.decode(tokens)
+        print(decoded)
+
+
+if __name__ == "__main__":
+    inference()
---
+++ model.py
@@ -0,0 +1,183 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import math
+from dataclasses import dataclass
+
+# GPT-2 (124M) model from scratch, taken from Karpathy's NanoGPT.
+
+@dataclass
+class Config:
+    block_size: int = 1024  # maximum sequence length
+    vocab_size: int = 50257  # number of tokens in our vocabulary
+    n_layer: int = 12
+    n_head: int = 12
+    n_embeddings: int = 768  # embedding dimensions
+
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+        assert config.n_embeddings % config.n_head == 0
+
+        self.c_attn = nn.Linear(config.n_embeddings, 3 * config.n_embeddings)
+        self.c_proj = nn.Linear(config.n_embeddings, config.n_embeddings)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embeddings
+
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size
+            ),
+        )
+
+    def forward(self, x):
+        B, T, C = x.size()  # batch size, sequence length and embedding dim
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        attention = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        # apply the mask
+        attention = attention.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+        attention = F.softmax(attention, dim=-1)
+
+        y = attention @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
+
+
+class MLP(nn.Module):
+    """
+    A Multi-Layer Perceptron.
+    Linear layer -> GELU activation -> Linear layer.
+    """
+
+    def __init__(self, config: Config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embeddings, 4 * config.n_embeddings)
+        self.gelu = nn.GELU(approximate="tanh")  # the approximate version of GELU
+        self.c_proj = nn.Linear(4 * config.n_embeddings, config.n_embeddings)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class AttentionBlock(nn.Module):
+    """
+    Attentionb block of in GPT-2 model.
+    Composed of layer normalization, a self-attention layer, and another layer normalization.
+    Finally, a MLP.
+    """
+
+    def __init__(self, config: Config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embeddings)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embeddings)
+        self.mlp = MLP(config)
+
+    def forward(self, x) -> torch.tensor:
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+class GPT(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(config.vocab_size, config.n_embeddings),
+                wpe=nn.Embedding(config.block_size, config.n_embeddings),
+                h=nn.ModuleList(
+                    [AttentionBlock(config) for _ in range(config.n_layer)]
+                ),
+                ln_f=nn.LayerNorm(config.n_embeddings),
+            )
+        )
+        self.lm_head = nn.Linear(config.n_embeddings, config.vocab_size, bias=False)
+
+    def forward(self, idx) -> torch.Tensor:
+        # token idx (batch, sequence length)
+        B, T = idx.size()
+        assert T <= self.config.block_size, "Problem with T: larger than block size"
+
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+        token_embeddings = self.transformer.wte(idx)
+        position_embeddings = self.transformer.wpe(pos)
+
+        x: torch.Tensor = position_embeddings + token_embeddings
+
+        for block in self.transformer.h:
+            x = block(x)
+
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x)
+        return logits
+
+    @classmethod
+    def from_pretrained(cls, model_type: str):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+        from transformers import GPT2LMHeadModel
+
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768),  # 124M
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),  # 350M
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),  # 774M
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M
+        }[model_type]
+
+        config_args["vocab_size"] = 50257  # always 50257 for GPT model checkpoints
+        config_args["block_size"] = 1024  # always 1024 for GPT model checkpoints
+        config_args["n_embeddings"] = config_args["n_embd"]
+        del config_args["n_embd"]
+
+        config = Config(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith(".attn.bias")]
+
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.masked_bias")]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.bias")]
+        transposed = [
+            "attn.c_attn.weight",
+            "attn.c_proj.weight",
+            "mlp.c_fc.weight",
+            "mlp.c_proj.weight",
+        ]
+
+        assert len(sd_keys_hf) == len(sd_keys), (
+            f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        )
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
